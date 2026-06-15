5871b98f390f7500db45c1700ec1cec419c6d3a6
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-06-15 21:10:38 +0600

    implemented admin logout button

---
+++ AdminDashboard.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DineMaster
+{
+    public partial class AdminDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Role"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                lblAdminName.Text = Session["StaffName"].ToString();
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
