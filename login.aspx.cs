bc8add362ccc04de937e0e5b5a8e0d54ebc53e3d
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-06-15 21:10:05 +0600

    implemented login button

---
+++ login.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DineMaster
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString
+        );
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+
+                string query = "SELECT staff_id, staff_name, role FROM STAFF WHERE username=@username AND password=@password";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    // session values
+                    Session["StaffID"] = dr["staff_id"].ToString();
+                    Session["StaffName"] = dr["staff_name"].ToString();
+                    Session["Role"] = dr["role"].ToString();
+
+                    string role = dr["role"].ToString();
+
+                    if (role == "Admin")
+                    {
+                        Response.Redirect("AdminDashboard.aspx");
+                    }
+                    else
+                    {
+                        Response.Redirect("StaffDashboard.aspx");
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "Invalid username or password!";
+                }
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+}
