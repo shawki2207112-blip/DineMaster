2d1dfb95aec8ee96d16fe858146ec228e111cf72
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-06-26 12:50:59 +0600

    Added the backend of staff dashboard

---
+++ StaffDashboard.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DineMaster
+{
+    public partial class StaffDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Role"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (Session["Role"].ToString() != "Staff")
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                lblStaffName.Text =
+                    Session["StaffName"].ToString();
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
