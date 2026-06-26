cd26a54e8be9f51fcdee8696aec544d636405f50
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-06-26 12:15:33 +0600

    Completed edit-delete feature

--- ManageStaff.aspx.cs
+++ ManageStaff.aspx.cs
@@ -39,6 +39,85 @@ namespace DineMaster
             
         }
 
+        protected void gvStaff_RowCommand(object sender,
+            GridViewCommandEventArgs e)
+        {
+            int rowIndex =
+                Convert.ToInt32(e.CommandArgument);
+
+            int staffID =
+                Convert.ToInt32(
+                gvStaff.DataKeys[rowIndex].Value);
+
+            if (e.CommandName == "DeleteRow")
+            {
+                con.Open();
+
+                OracleCommand cmd =
+                    new OracleCommand(
+                    "DELETE FROM STAFF WHERE staff_id = :staffId",
+                    con);
+
+                cmd.BindByName = true;
+
+                cmd.Parameters.Add(":staffId", staffID);
+
+                cmd.ExecuteNonQuery();
+
+                OracleCommand commitCmd =
+                    new OracleCommand("COMMIT", con);
+
+                commitCmd.ExecuteNonQuery();
+
+                con.Close();
+
+                LoadStaff();
+
+                lblMessage.Text = "Staff Deleted Successfully";
+            }
+
+            if (e.CommandName == "EditRow")
+            {
+                con.Open();
+
+                OracleCommand cmd =
+                    new OracleCommand(
+                    "SELECT * FROM STAFF WHERE staff_id = :staffId",
+                    con);
+
+                cmd.BindByName = true;
+
+                cmd.Parameters.Add(":staffId", staffID);
+
+                OracleDataReader dr =
+                    cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    hfStaffID.Value =
+                        dr["staff_id"].ToString();
+
+                    txtStaffName.Text =
+                        dr["staff_name"].ToString();
+
+                    txtPhone.Text =
+                        dr["phone"].ToString();
+
+                    txtSalary.Text =
+                        dr["salary"].ToString();
+
+                    txtUsername.Text =
+                        dr["username"].ToString();
+
+                    txtPassword.Text =
+                        dr["password"].ToString();
+                }
+
+                dr.Close();
+                con.Close();
+            }
+        }
+
         protected void btnClear_Click(object sender, EventArgs e)
         {
             ClearFields();
