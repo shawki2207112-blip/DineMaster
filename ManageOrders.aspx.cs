14ee30997a324ef400ed0d7e4b25650285492add
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-07-11 10:21:20 +0600

    Implement navigation process from Order Feature

---
+++ ManageOrders.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DineMaster
+{
+    public partial class ManageOrders : System.Web.UI.Page
+    {
+        string connectionString =
+            ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Role"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!Session["Role"].ToString().Equals("Staff", StringComparison.OrdinalIgnoreCase))
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (Session["StaffID"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = ex.Message;
+                }
+            }
+        }
+
+       
